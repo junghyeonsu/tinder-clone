1f794bc82e1b5f5d7bee255d530a21a5e11affae
junghyeonsu <54893898+junghyeonsu@users.noreply.github.com>
2020-08-22 14:21:36 +0900

    create-react-app 구축

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Let's build the TINDER Clone App!</h1>
+    </div>
+  );
+}
+
+export default App;
