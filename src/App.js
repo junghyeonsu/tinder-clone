8c97453dd3e43690e959f0d67134bab0853b7a12
junghyeonsu <54893898+junghyeonsu@users.noreply.github.com>
2020-08-30 17:12:33 +0900

    Header 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,57 +1,24 @@
 import React, { Component } from 'react';
+import Header from './Header';
 import './App.css';
 import { firestore } from "./firebase";
 import { Button } from 'react-bootstrap';
 
 class App extends Component {
-  state = {
-    data : 0
-  }
-
-  componentDidMount(){
-    firestore.collection("test")
-    .doc("friend")
-    .get()
-    .then(doc => {
-      this.setState({
-        data : doc.data().friend_count
-      }) 
-    })
-  }
-  
-  onClickUpButton = () => {  
-    firestore.collection("test")
-    .doc("friend")
-    .update({
-      friend_count : this.state.data + 1
-    })
-    .then(
-      this.setState({
-        data : this.state.data + 1
-      })
-    )
-  }
-
-  onClickDownButton = () => {
-    firestore.collection("test")
-    .doc("friend")
-    .update({
-      friend_count : this.state.data - 1
-    })
-    .then(
-      this.setState({
-        data : this.state.data - 1
-      })
-    )
-  }
-
   render(){
     return (
       <div className="App">
-        <h1>Let's build the TINDER Clone App!</h1>
-        <h2> 현재 친구는 {this.state.data}명 입니다.</h2>
-        <Button variant="primary" onClick={this.onClickUpButton}>친구 늘리기</Button><br />
-        <Button variant="danger"  onClick={this.onClickDownButton}>친구 줄이기</Button>
+        <h1>This is awesome TINDER Clone App</h1>
+
+        {/* Header */}
+        <Header />
+
+        {/* Tinder Cards */}
+        {/* Buttons belos tinder cards */}
+
+        {/* Chats screen */}
+        {/* Individual Chat screen */}
+
       </div>
     );
   }
---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import PersonIcon from '@material-ui/icons/Person';
+import ForumIcon from '@material-ui/icons/Forum';
+
+function Header() {
+    return (
+        <div>
+            <PersonIcon />
+            <h2>I am a header</h2>
+            <ForumIcon />
+        </div>
+    )
+}
+
+export default Header
