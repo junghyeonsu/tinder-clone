1781fb9032c377f1caba8a7a9fa8ded968a55747
junghyeonsu <54893898+junghyeonsu@users.noreply.github.com>
2020-08-30 23:38:25 +0900

    Header 완성

--- src/App.js
+++ src/App.js
@@ -8,9 +8,6 @@ class App extends Component {
   render(){
     return (
       <div className="App">
-        <h1>This is awesome TINDER Clone App</h1>
-
-        {/* Header */}
         <Header />
 
         {/* Tinder Cards */}
--- src/Header.js
+++ src/Header.js
@@ -1,15 +1,26 @@
 import React from 'react'
+import "./Header.css";
 import PersonIcon from '@material-ui/icons/Person';
 import ForumIcon from '@material-ui/icons/Forum';
+import IconButton from "@material-ui/core/IconButton";
 
 function Header() {
     return (
-        <div>
-            <PersonIcon />
-            <h2>I am a header</h2>
-            <ForumIcon />
+        <div className="header">
+            <IconButton>
+                <PersonIcon className="header__icon" fontSize="large" />
+            </IconButton>
+            <img
+                className="header__logo" 
+                src="https://1000logos.net/wp-content/uploads/2018/07/tinder-logo.png" 
+                alt="tinder logo" 
+            />
+            <IconButton>
+                <ForumIcon className="header__icon" fontSize="large" />
+            </IconButton>
         </div>
     )
 }
 
 export default Header
+
