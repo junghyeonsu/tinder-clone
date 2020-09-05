e0f4df131aba7cedf2a3faa647d252f50bcb2029
junghyeonsu <54893898+junghyeonsu@users.noreply.github.com>
2020-09-05 16:15:08 +0900

    Firebase 연동

--- src/TinderCards.js
+++ src/TinderCards.js
@@ -1,18 +1,18 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import database from './firebase';
 import TinderCard from "react-tinder-card";
 import './TinderCards.css';
 
 function TinderCards() {
-    const [people, setPeople] = useState([
-        {
-            name: 'steve jobs',
-            url: 'https://www.biography.com/.image/t_share/MTY2MzU3OTcxMTUwODQxNTM1/steve-jobs--david-paul-morrisbloomberg-via-getty-images.jpg'
-        },
-        {
-            name: 'mark zuckerberg',
-            url: 'https://api.time.com/wp-content/uploads/2019/04/mark-zuckerberg-time-100-2019.jpg?quality=85&zoom=2'
-        }
-    ]);
+    const [people, setPeople] = useState([]);
+
+    useEffect(() => {
+        
+        database.collection('people').onSnapshot(snapshot => {
+            setPeople(snapshot.docs.map(doc => doc.data()))
+        })
+
+    }, []);
 
     return (
         <div>
